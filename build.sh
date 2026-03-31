bcc3db53252ed35fdf394a8e9d8076e0f2d67d08
prosperReigns <prosperkinging7@gmail.com>
2026-03-31 12:21:52 +0100

    update

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ set -o errexit
 # Go to backend folder
 cd backend
 
+# Upgrade pip first
+python -m pip install --upgrade pip
+
 # Install dependencies
 pip install -r requirements.txt
 
@@ -11,4 +14,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 # Run migrations
-python manage.py migratee
+python manage.py migrate
