eaf461991c93bd84fe7508b80eaa92e63f7dc434
prosperReigns <prosperkinging7@gmail.com>
2026-03-31 12:00:51 +0100

    update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Go to backend folder
+cd backend
+
+# Install dependencies
 pip install -r requirements.txt
+
+# Collect static files
 python manage.py collectstatic --no-input
-python manage.py migrate
+
+# Run migrations
+python manage.py migratee
